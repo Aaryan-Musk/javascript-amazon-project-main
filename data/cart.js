812ad642231ecb6a3df7cfc2ab3efb857e97b839
AaryanAgarwalDev <newtonaaryan7@gmail.com>
2026-04-22 14:49:04 +0530

    delete products from cart when clicking delete

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,22 @@
-export const cart = [{
+export let cart = [{
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2,
 }, {
     productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
     quantity: 1,
 }];
+
+export function removeFromCart(productId) {
+    const newCart = [];
+
+    cart.forEach((CartItem) => {
+        if(CartItem.productId != productId){
+            newCart.push(CartItem);
+        }
+    });
+
+    cart = newCart;
+}
 
 export function addToCart(productId) {
 
